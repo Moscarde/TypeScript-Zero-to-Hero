4d971be077a3bda213984e92de909cd6bd7e1cd7
Moscarde <theizera@gmail.com>
2023-05-03 23:01:18 -0300

    03.05

--- modulo-02/32-aula-tipo-object/objectType.ts
+++ modulo-02/32-aula-tipo-object/objectType.ts
@@ -11,20 +11,20 @@ export { };
 // ==> Exemplo 01 -> Exemplo básico do uso do Type Object
 
 const pessoa = {
-  nome: 'Glaucia',
-  sobrenome: 'Lemos',
-  idade: 35,
-  funcao: 'Cloud Advocate'
+  nome: 'Gabriel',
+  sobrenome: 'Moscarde',
+  idade: 25,
+  funcao: 'Vendedor'
 };
 
 console.log(pessoa);
 
 // ==> Exemplo 02 -> object como parâmetros de função (eles podem ser anônimos)
 function onboarding01(funcionario: { nome: string }) {
-  return 'Seja bem-vinda ' + funcionario.nome;
+  return 'Seja bem-vindo ' + funcionario.nome;
 }
 
-console.log(onboarding01({ nome: 'Glaucia Lemos' }));
+console.log(onboarding01({ nome: 'Gabriel Moscarde' }));
 
 // ==> Exemplo 03 -> object nomeados
 
@@ -44,7 +44,7 @@ function onboarding02(pessoa: Pessoa) {
   );
 }
 
-console.log(onboarding02({ nome: 'Glaucia Lemos', funcao: 'Cloud Advocate' }));
+console.log(onboarding02({ nome: 'Gabriel Moscarde', funcao: 'Vendedor' }));
 
 // ==> Exemplo 04 -> object como type alias
 
@@ -68,7 +68,7 @@ function onboarding03(pessoa: Pessoa03) {
   );
 }
 
-console.log(onboarding03({ nome: 'Glaucia Lemos', funcao: 'Cloud Advocate', linguagem: 'JavaScript/Typescript' }));
+console.log(onboarding03({ nome: 'Gabriel Moscarde', funcao: 'Vendedor', linguagem: 'JavaScript/Typescript' }));
 
 
 // ==> Exemplo 05 -> usando optional no object
@@ -96,107 +96,107 @@ function onboarding04(pessoa: Pessoa04) {
 
 console.log(onboarding04({ nome: 'Glaucia Lemos', funcao: 'Cloud Advocate', linguagem: 'JavaScript/Typescript' }));
 
-// ==> Exemplo 06 -> Propriedade 'readonly' (se deseja proibir que os devs não modifiquem um determinado objeto use o '
-//readonly)
+// // ==> Exemplo 06 -> Propriedade 'readonly' (se deseja proibir que os devs não modifiquem um determinado objeto use o '
+// //readonly)
 
-interface Pessoa05 {
-  nome: string;
-  funcao: string;
-  linguagem: string;
-  readonly email: string;
-}
+// interface Pessoa05 {
+//   nome: string;
+//   funcao: string;
+//   linguagem: string;
+//   readonly email: string;
+// }
 
-function onboarding05(pessoa: Pessoa05) {
-  return (
-    'Seja bem-vinda ' +
-    pessoa.nome +
-    '!' +
-    ' Sua função aqui na empresa será ' +
-    pessoa.funcao +
-    '.' +
-    ' Você trabalhará com a linguagem ' +
-    pessoa.linguagem +
-    '.' +
-    ' Seu e-mail será ' +
-    pessoa.email
-  );
-}
+// function onboarding05(pessoa: Pessoa05) {
+//   return (
+//     'Seja bem-vinda ' +
+//     pessoa.nome +
+//     '!' +
+//     ' Sua função aqui na empresa será ' +
+//     pessoa.funcao +
+//     '.' +
+//     ' Você trabalhará com a linguagem ' +
+//     pessoa.linguagem +
+//     '.' +
+//     ' Seu e-mail será ' +
+//     pessoa.email
+//   );
+// }
 
-console.log(onboarding05(
-  {
-    nome: 'Glaucia Lemos',
-    funcao: 'Cloud Advocate',
-    linguagem: 'JavaScript/Typescript',
-    email: 'gllemos@microsoft.com'
-  }
-));
+// console.log(onboarding05(
+//   {
+//     nome: 'Glaucia Lemos',
+//     funcao: 'Cloud Advocate',
+//     linguagem: 'JavaScript/Typescript',
+//     email: 'gllemos@microsoft.com'
+//   }
+// ));
 
-// ==> Exemplo 07 -> tipos de extensões (heranças)
-interface Mae {
-  nome: string;
-}
+// // ==> Exemplo 07 -> tipos de extensões (heranças)
+// interface Mae {
+//   nome: string;
+// }
 
-interface Pai {
-  sobrenome: string;
-}
+// interface Pai {
+//   sobrenome: string;
+// }
 
-interface Filha extends Mae, Pai {
-  idade: number;
-}
+// interface Filha extends Mae, Pai {
+//   idade: number;
+// }
 
-const filha: Filha = {
-  nome: 'Glaucia',
-  sobrenome: 'Lemos',
-  idade: 35
-}
+// const filha: Filha = {
+//   nome: 'Glaucia',
+//   sobrenome: 'Lemos',
+//   idade: 35
+// }
 
-console.log(filha);
+// console.log(filha);
 
-// ==> Exemplo 08 -> Tipos de Interseções
+// // ==> Exemplo 08 -> Tipos de Interseções
 
-interface Cachorro {
-  tipo: string;
-}
+// interface Cachorro {
+//   tipo: string;
+// }
 
-interface Gato {
-  tipo: string;
-}
+// interface Gato {
+//   tipo: string;
+// }
 
-type Animal = Cachorro & Gato;
+// type Animal = Cachorro & Gato;
 
-// Exemplo 09 - Generic Objects
+// // Exemplo 09 - Generic Objects
 
-type Usuario = {
-  nome: string;
-  email: string;
-}
+// type Usuario = {
+//   nome: string;
+//   email: string;
+// }
 
-type Admin = {
-  nome: string;
-  email: string;
-  admin: true;
-}
+// type Admin = {
+//   nome: string;
+//   email: string;
+//   admin: true;
+// }
 
-const usuario: Usuario = {
-  nome: 'Glaucia Lemos',
-  email: 'algumacoisa@gmail.com'
-}
+// const usuario: Usuario = {
+//   nome: 'Glaucia Lemos',
+//   email: 'algumacoisa@gmail.com'
+// }
 
-const admin: Admin = {
-  nome: 'Glaucia Lemos',
-  email: 'algumacoisa@gmail.com',
-  admin: true
-}
+// const admin: Admin = {
+//   nome: 'Glaucia Lemos',
+//   email: 'algumacoisa@gmail.com',
+//   admin: true
+// }
 
-function acessarSistema<T>(usuario: T): T {
-  return usuario;
-}
+// function acessarSistema<T>(usuario: T): T {
+//   return usuario;
+// }
 
-console.log(acessarSistema<Usuario>(usuario));
-console.log(acessarSistema<Admin>(admin));
+// console.log(acessarSistema<Usuario>(usuario));
+// console.log(acessarSistema<Admin>(admin));
 
-/*function acessarSistema(usuario: Usuario): Usuario {
-  return usuario;
-};*/
+// /*function acessarSistema(usuario: Usuario): Usuario {
+//   return usuario;
+// };*/
 
-// console.log(acessarSistema(usuario));
+// // console.log(acessarSistema(usuario));
